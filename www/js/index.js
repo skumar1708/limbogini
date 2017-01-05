bfb86b345c996d961760bda4769ccc17c0658471
Sharvan <itbhu.sk@gmail.com>
2017-01-05 13:40:58 +0530

    Add files via upload

---
+++ www/js/index.js
@@ -0,0 +1,101 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+		
+		$("a").on('click',this.btnClick);
+		
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+		document.addEventListener('backbutton', onBackKeyDown); //Listen to the User clicking on the back button	
+        app.receivedEvent('deviceready');
+    },
+	
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    },
+	btnClick:function(){
+		   
+       //$("iframe").contents().find(".yt-uix-sessionlink").hide();
+		
+		var vid = $(event.currentTarget).attr('id');
+		jQuery("#youtube-player").tubeplayer('play',vid);
+		
+		/* var player = new MediaElementPlayer('#video');
+		
+		
+			player.setSrc('https://www.youtube.com/watch?v='+vid);
+			player.play(); */
+		$('img').removeClass('rightShift');
+		$('img').attr('src','img/play.png');
+		 console.log($(this).find('img').attr('src'));
+		/* var vdo = document.getElementById('video'); */
+		/* var vsrc = document.getElementById('vsrc');
+		vsrc.src = 'https://www.youtube.com/watch?v='+vid;
+		
+		player.play(); */
+		
+		/* $('video').mediaelementplayer(); */
+		/* vdo.load(); */ 
+		
+		/* vdo.play(); */
+		$(this).find('img').attr('src','img/pause.png');
+		$(this).find('img').addClass('rightShift');
+		document.getElementsByClassName('ytp-watermark')[0].style.display = 'none';
+	}
+};
+
+		
+
+	function onBackKeyDown(e) {
+		alert(123);
+		e.preventDefault();
+		navigator.notification.confirm("Are you sure you want to exit ?", this.onConfirm, "Confirmation", "Yes,No"); 
+		// Prompt the user with the choice
+	}
+
+	function onConfirm(button) {
+		if(button==2){//If User selected No, then we just do nothing
+			return;
+		}else{
+			navigator.app.exitApp();// Otherwise we quit the app.
+		}
+	}
+
